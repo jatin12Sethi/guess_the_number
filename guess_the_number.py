ce6e53ca3463fe5a11cb0a76b9df579676f5b1f9
Akshit Gandotra <90208584+akshit-g@users.noreply.github.com>
2022-06-02 20:27:54 +0530

    Add files via upload

---
+++ guess_the_number.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+import random
+
+# creating window
+win = tk.Tk()
+
+# define window dimension
+win.geometry("700x400")
+
+# make window resizable (default = true)
+win.resizable(width=True, height=True)
+
+# setting a background color
+win.config(bg="#FDE6E5")
+
+# setting the window title
+win.title("Guess the Number")
+
