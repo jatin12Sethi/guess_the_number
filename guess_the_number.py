3f8f13e0bb2521d3d3e59f3acb54d2ffc2461f59
Akshit Gandotra <90208584+akshit-g@users.noreply.github.com>
2022-06-02 21:40:30 +0530

    Add files via upload

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import random
+from turtle import color
 
 # creating window
 win = tk.Tk()
@@ -16,3 +17,45 @@ win.config(bg="#FDE6E5")
 # setting the window title
 win.title("Guess the Number")
 
+# making the main GUI
+# setting the lower and upper bound
+lower = int(input("Enter the lower bound:\t"))
+upper = int(input("Enter the upper bound:\t"))
+
+
+target = random.randint(lower, upper)
+retry_attempt = 0
+
+def update_result(text):
+    result.configure(text=text)
+    
+# creating new game
+def new_game():
+    guess_button.configure(state="normal")
+    global target, retry_attempt
+    target = random.randint(lower, upper)
+    retry_attempt = 0
+    update_result(text="Guess a number between\n1 and 100")
+
+# continue or terminate
+def play_game():
+    global retry_attempt
+    
+    choice = int(number_enter.get())
+    
+    if choice != target:
+        retry_attempt += 1
+        
+        result = "Your guess is incorrect. Try Again!"
+        if target < choice:
+            hint = "The number lies between 0 & {}".format(result)
+            
+        else:
+            hint = "The number lies between {} & 1000".format(choice)
+        result += "\n\nHint:\n" + hint
+        
+    else:
+        result = "You guess it coreectly in {} tries".format(retry_attempt)
+        guess_button.configure(state="disabled")
+        result += "\n\nHint:\n" + "Click 'Play' to start a new round"
+        
